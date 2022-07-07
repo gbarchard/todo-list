a658e35f861c7df7c0214c2c002223119d9e887c
Taylor Larsen <taylor.sh.larsen@gmail.com>
2022-07-07 18:51:50 -0400

    Remove firebase creds

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -3,15 +3,7 @@ import { getAnalytics } from "firebase/analytics"
 import { getAuth } from "firebase/auth"
 
 // TODO: Add your config here...
-const firebaseConfig = {
-  apiKey: "AIzaSyCBy68fPiUg4U8yExaKBKxCqK0MXPs33aQ",
-  authDomain: "etch-dev-2d703.firebaseapp.com",
-  projectId: "etch-dev-2d703",
-  storageBucket: "etch-dev-2d703.appspot.com",
-  messagingSenderId: "986675990728",
-  appId: "1:986675990728:web:55146c04565cf8c473d5fd",
-  measurementId: "G-KCNGNVTZLN",
-}
+const firebaseConfig = {}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)

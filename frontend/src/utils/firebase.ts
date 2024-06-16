a076facbc19c1ec11db6694cd7935d207a17f155
Grant Barchard <grant.barchard@gmail.com>
2024-06-16 16:58:00 -0400

    frontend firebase config

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,11 +1,10 @@
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-import { getAuth } from "firebase/auth"
-
-// TODO: Add your config here...
-const firebaseConfig = {}
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import firebaseConfig from './firebase.json'
 
 // Initialize Firebase
+if (!firebaseConfig) throw Error('Missing Firebase Config')
 const app = initializeApp(firebaseConfig)
 export const analytics = getAnalytics(app)
 export const auth = getAuth(app)
